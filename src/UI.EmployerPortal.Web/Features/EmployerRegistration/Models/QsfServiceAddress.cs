2a1c5eebf92b88294fd9244ee4df855b18524c20
Kaushik <kaushikrathod4u@gmail.com>
2026-04-06 11:55:35 +0530

    replace ValidateInput with OutlineTextInput

--- src/UI.EmployerPortal.Web/Features/EmployerRegistration/Models/QsfServiceAddress.cs
+++ src/UI.EmployerPortal.Web/Features/EmployerRegistration/Models/QsfServiceAddress.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace UI.EmployerPortal.Web.Features.EmployerRegistration.Models;
 
 /// <summary>
@@ -6,18 +8,22 @@ namespace UI.EmployerPortal.Web.Features.EmployerRegistration.Models;
 public class QsfServiceAddress
 {
     /// <summary>Address Line 1.</summary>
+    [Required(ErrorMessage = "Address Line 1 is required")]
     public string? AddressLine1 { get; set; }
 
     /// <summary>Address Line 2 (optional).</summary>
     public string? AddressLine2 { get; set; }
 
     /// <summary>City.</summary>
+    [Required(ErrorMessage = "City is required")]
     public string? City { get; set; }
 
     /// <summary>State (defaults to Wisconsin).</summary>
     public string State { get; set; } = "Wisconsin";
 
     /// <summary>Zip code (5 digits).</summary>
+    [Required(ErrorMessage = "Zip Code is required")]
+    [RegularExpression(@"^\d{5}$", ErrorMessage = "Zip Code must be 5 digits")]
     public string? ZipCode { get; set; }
 
     /// <summary>Zip extension (optional, 4 digits).</summary>
--- src/UI.EmployerPortal.Web/Features/EmployerRegistration/Models/QualifiedSettlementFundData.cs
+++ src/UI.EmployerPortal.Web/Features/EmployerRegistration/Models/QualifiedSettlementFundData.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace UI.EmployerPortal.Web.Features.EmployerRegistration.Models;
 
 /// <summary>
@@ -13,9 +15,11 @@ public class QualifiedSettlementFundModel
     // --- "Yes" branch ---
 
     /// <summary>Legal name of the entity that received the services.</summary>
+    [Required(ErrorMessage = "Legal name is required")]
     public string? EntityLegalName { get; set; }
 
     /// <summary>Federal Employer Identification Number of the entity.</summary>
+    [Required(ErrorMessage = "Federal ID Number is required")]
     public string? FederalIdNumber { get; set; }
 
     /// <summary>Wisconsin UI Account Number (optional).</summary>
