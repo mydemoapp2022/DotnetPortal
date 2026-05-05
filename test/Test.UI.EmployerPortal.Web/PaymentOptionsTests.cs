c30689ef686e91b13a6b06c26fbcdcdc2bc20dd9
Kaushik <kaushikrathod4u@gmail.com>
2026-05-05 20:37:18 +0530

    modify test cases for Payment Options

--- test/Test.UI.EmployerPortal.Web/PaymentOptionsTests.cs
+++ test/Test.UI.EmployerPortal.Web/PaymentOptionsTests.cs
@@ -1,9 +1,11 @@
+using System.Reflection;
 using Bunit;
 using FakeItEasy;
 using Microsoft.Extensions.DependencyInjection;
 using UI.EmployerPortal.Web.Features.BillingPayments.Components;
 using UI.EmployerPortal.Web.Features.BillingPayments.Models;
 using UI.EmployerPortal.Web.Features.BillingPayments.Pages;
+using Xunit;
 
 namespace Test.UI.EmployerPortal.Web;
 
@@ -13,9 +15,51 @@ public class PaymentOptionsTests : BunitContext
     {
     }
 
-    private IRenderedComponent<BillingPayments> RenderComponent()
+    private IRenderedComponent<PaymentOptions> RenderComponent()
     {
-        return Render<BillingPayments>();
+        return Render<PaymentOptions>();
+    }
+
+    private static async Task<bool> InvokeValidateAsync(object instance)
+    {
+        var method = instance.GetType().GetMethod(
+            "Validate",
+            BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+        Assert.NotNull(method);
+
+        var result = method!.Invoke(instance, null);
+
+        return result switch
+        {
+            Task<bool> task => await task,
+            bool value => value,
+            _ => throw new InvalidOperationException("Validate must return bool or Task<bool>.")
+        };
+    }
+
+    [Fact]
+    public void Renders_Billing_And_Payments_Page()
+    {
+        var cut = RenderComponent();
+
+        Assert.Contains("Billing & Payments", cut.Markup);
+    }
+
+    [Fact]
+    public void Renders_Billing_Component()
+    {
+        var cut = RenderComponent();
+
+        Assert.NotEmpty(cut.FindAll("div.section-page"));
+    }
+
+    [Fact]
+    public void Accepts_Source_Query_Parameter_Flow()
+    {
+        var cut = RenderComponent("flow");
+
+        Assert.Contains("Billing & Payments", cut.Markup);
     }
 
     [Fact]
@@ -36,7 +80,9 @@ public class PaymentOptionsTests : BunitContext
     public async Task Validate_Returns_False_When_No_Option_Selected()
     {
         var cut = RenderComponent();
-        var result = await cut.InvokeAsync(cut.Instance.Validate);
+
+        var result = await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
+
         Assert.False(result);
     }
 
@@ -44,20 +90,23 @@ public class PaymentOptionsTests : BunitContext
     public async Task Validate_Shows_Required_Error_When_No_Option_Selected()
     {
         var cut = RenderComponent();
-        await cut.InvokeAsync(cut.Instance.Validate);
-        Assert.Contains("Payment option is required.", cut.Markup);
+
+        await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
+
+        Assert.Contains(RequiredMessage, cut.Markup);
     }
 
     [Fact]
     public async Task Validate_Returns_True_When_Any_Option_Selected()
     {
         var cut = RenderComponent();
+
         var radios = cut.FindAll("input[type='radio']");
         Assert.NotEmpty(radios);
 
         radios[0].Change(true);
 
-        var result = await cut.InvokeAsync(cut.Instance.Validate);
+        var result = await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
 
         Assert.True(result);
     }
@@ -67,17 +116,17 @@ public class PaymentOptionsTests : BunitContext
     {
         var cut = RenderComponent();
 
-        await cut.InvokeAsync(cut.Instance.Validate);
-        Assert.Contains("Payment option is required.", cut.Markup);
+        await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
+        Assert.Contains(RequiredMessage, cut.Markup);
 
         var radios = cut.FindAll("input[type='radio']");
         Assert.NotEmpty(radios);
 
         radios[0].Change(true);
 
-        var result = await cut.InvokeAsync(cut.Instance.Validate);
+        var result = await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
 
         Assert.True(result);
-        Assert.DoesNotContain("Payment option is required.", cut.Markup);
+        Assert.DoesNotContain(RequiredMessage, cut.Markup);
     }
 }
