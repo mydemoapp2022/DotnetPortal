4000df3fae75395739694226acedba3a7c0a7192
Kaushik <kaushikrathod4u@gmail.com>
2026-05-05 17:04:30 +0530

    show validation summary Back button visibility as per navigation

---
+++ src/UI.EmployerPortal.Web/Features/BillingPayments/Models/NavigationSource.cs
@@ -0,0 +1,12 @@
+namespace UI.EmployerPortal.Web.Features.BillingPayments.Models;
+
+/// <summary>
+/// NavigationSource
+/// </summary>
+public enum NavigationSource
+{
+    /// <summary> Menu </summary>
+    Menu,
+    /// <summary> Flow </summary>
+    Flow
+}
