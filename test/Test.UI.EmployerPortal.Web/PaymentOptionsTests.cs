a89ae1556cf14f3e01d5e63cfe799ff353f5b689
Kaushik <kaushikrathod4u@gmail.com>
2026-05-06 00:00:37 +0530

    resolve all errors of PaymentOptionsTests

--- test/Test.UI.EmployerPortal.Web/PaymentOptionsTests.cs
+++ test/Test.UI.EmployerPortal.Web/PaymentOptionsTests.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using Bunit;
 using FakeItEasy;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 using UI.EmployerPortal.Web.Features.BillingPayments.Components;
 using UI.EmployerPortal.Web.Features.BillingPayments.Models;
@@ -11,122 +12,62 @@ namespace Test.UI.EmployerPortal.Web;
 
 public class PaymentOptionsTests : BunitContext
 {
-    public PaymentOptionsTests()
+    private const string RequiredMessage = "Payment option is required.";
+    private IRenderedComponent<BillingPaymentsMain> RenderComponent()
     {
-    }
-
-    private IRenderedComponent<PaymentOptions> RenderComponent()
-    {
-        return Render<PaymentOptions>();
-    }
-
-    private static async Task<bool> InvokeValidateAsync(object instance)
-    {
-        var method = instance.GetType().GetMethod(
-            "Validate",
-            BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-        Assert.NotNull(method);
-
-        var result = method!.Invoke(instance, null);
-
-        return result switch
+        return Render<BillingPaymentsMain>(p =>
         {
-            Task<bool> task => await task,
-            bool value => value,
-            _ => throw new InvalidOperationException("Validate must return bool or Task<bool>.")
-        };
+            p.Add(x => x.Source, NavigationSource.Menu);
+        });
     }
 
     [Fact]
-    public void Renders_Billing_And_Payments_Page()
+    public void Validate_Returns_False_When_No_Option_Selected()
     {
         var cut = RenderComponent();
 
-        Assert.Contains("Billing & Payments", cut.Markup);
-    }
-
-    [Fact]
-    public void Renders_Billing_Component()
-    {
-        var cut = RenderComponent();
-
-        Assert.NotEmpty(cut.FindAll("div.section-page"));
-    }
-
-    [Fact]
-    public void Accepts_Source_Query_Parameter_Flow()
-    {
-        var cut = RenderComponent("flow");
-
-        Assert.Contains("Billing & Payments", cut.Markup);
-    }
-
-    [Fact]
-    public void Renders_Page_Title()
-    {
-        var cut = RenderComponent();
-        Assert.Contains("Payment Options", cut.Markup);
-    }
-
-    [Fact]
-    public void Error_Banner_Not_Visible_By_Default()
-    {
-        var cut = RenderComponent();
-        Assert.DoesNotContain("Payment option is required.", cut.Markup);
-    }
-
-    [Fact]
-    public async Task Validate_Returns_False_When_No_Option_Selected()
-    {
-        var cut = RenderComponent();
-
-        var result = await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
-
-        Assert.False(result);
-    }
-
-    [Fact]
-    public async Task Validate_Shows_Required_Error_When_No_Option_Selected()
-    {
-        var cut = RenderComponent();
-
-        await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
+        cut.Find("button[type='button'].btn-primary").Click();
 
         Assert.Contains(RequiredMessage, cut.Markup);
     }
 
     [Fact]
-    public async Task Validate_Returns_True_When_Any_Option_Selected()
+    public void Validate_Shows_Required_Error_When_No_Option_Selected()
     {
         var cut = RenderComponent();
 
-        var radios = cut.FindAll("input[type='radio']");
-        Assert.NotEmpty(radios);
+        cut.Find("button[type='button'].btn-primary").Click();
 
-        radios[0].Change(true);
-
-        var result = await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
-
-        Assert.True(result);
+        Assert.Contains(RequiredMessage, cut.Markup);
     }
 
     [Fact]
-    public async Task Validate_Clears_Error_After_Selecting_An_Option()
+    public void Validate_Returns_True_When_Any_Option_Selected()
     {
         var cut = RenderComponent();
+        var nav = Services.GetRequiredService<NavigationManager>();
 
-        await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
+        cut.FindAll("div[role='radio']")[0].Click();
+        cut.Find("button[type='button'].btn-primary").Click();
+
+        Assert.EndsWith("/billing-payments/payment-history", nav.Uri);
+    }
+
+    [Fact]
+    public void Validate_Clears_Error_After_Selecting_An_Option()
+    {
+        var cut = RenderComponent();
+        var nav = Services.GetRequiredService<NavigationManager>();
+
+        // First attempt without selection -> shows validation
+        cut.Find("button[type='button'].btn-primary").Click();
         Assert.Contains(RequiredMessage, cut.Markup);
 
-        var radios = cut.FindAll("input[type='radio']");
-        Assert.NotEmpty(radios);
+        // Select option and continue again -> clears validation and navigates
+        cut.FindAll("div[role='radio']")[1].Click(); // Select Card
+        cut.Find("button[type='button'].btn-primary").Click();
 
-        radios[0].Change(true);
-
-        var result = await cut.InvokeAsync(() => InvokeValidateAsync(cut.Instance));
-
-        Assert.True(result);
         Assert.DoesNotContain(RequiredMessage, cut.Markup);
+        Assert.EndsWith("/billing-payments/payment-history", nav.Uri);
     }
 }
